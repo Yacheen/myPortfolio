5534fff3c9c45702dce5e907f3857663e61fb9c6
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-06-20 16:00:17 -0600

    finished styling and buttons

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const hamburger = document.getElementById('burger-button');
+const hamburger = document.querySelector('.fa-bars');
 const items = document.querySelector('.navigation-links');
-console.log(hamburger);
-console.log(items);
+const collectionOfItems = document.querySelectorAll('.items');
+
+//for each item in here, add event listener that if they click it, toggle code above
+
 
 hamburger.addEventListener('click', () => {
     items.classList.toggle('navigation-links-active');
     hamburger.classList.toggle('fa-times');
     
+    
+})
+collectionOfItems.forEach(item => {
+    item.addEventListener('click', () => {
+        items.classList.toggle('navigation-links-active');
+        hamburger.classList.toggle('fa-times');
+    })
 })
