75e14a1d79ea0db8bb70144a2e8521e465215343
Yassin <81421290+Yacheen@users.noreply.github.com>
2021-08-23 04:17:30 -0600

    Added animations to portfolio

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-const hamburger = document.querySelector('.fa-bars');
-const items = document.querySelector('.navigation-links');
-const collectionOfItems = document.querySelectorAll('.items');
+const hamburger = document.querySelector(".fa-bars");
+const items = document.querySelector(".navigation-links");
+const collectionOfItems = document.querySelectorAll(".items");
 
 //for each item in here, add event listener that if they click it, toggle code above
 
-
-hamburger.addEventListener('click', () => {
-    items.classList.toggle('navigation-links-active');
-    hamburger.classList.toggle('fa-times');
-    
-    
-})
-collectionOfItems.forEach(item => {
-    item.addEventListener('click', () => {
-        items.classList.toggle('navigation-links-active');
-        hamburger.classList.toggle('fa-times');
-    })
-})
+hamburger.addEventListener("click", () => {
+    items.classList.toggle("navigation-links-active");
+    hamburger.classList.toggle("fa-times");
+});
+collectionOfItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        items.classList.toggle("navigation-links-active");
+        hamburger.classList.toggle("fa-times");
+    });
+});
